0f41d7ef776da892a0da04b84f095cb967f393a5
wiktor <wiktor.kycia@gmail.com>
2025-09-23 16:49:17 +0200

    cout the main menu

--- pasja_informatyki/main_menu.cpp
+++ pasja_informatyki/main_menu.cpp
@@ -2,9 +2,29 @@
 using namespace std;
 
 float x, y;
+int wybor;
 
 int main ()
 {
+    setlocale(LC_ALL, "pl_PL.UTF-8");
+    cout << "Podaj 1 liczbę: " << endl;
+    cin >> x;
+    cout << "Podaj 2 liczbę: " << endl;
+    cin >> y;
+
+    cout << endl;
+    cout << "MENU GŁÓWNE" << endl;
+    cout << "===========" << endl;
+    cout << "1. Dodawanie" << endl;
+    cout << "2. Odejmowanie" << endl;
+    cout << "3. Mnożenie" << endl;
+    cout << "4. Dzielenie" << endl;
+
+    cout << "Wybierz: " ;
+    cin >> wybor;
+
     
+    
+
     return 0;
 }
