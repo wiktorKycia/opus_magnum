96a8aece07cd631853f8c0bca06c7a0bd70456aa
wiktor <wiktor.kycia@gmail.com>
2025-09-28 13:03:43 +0200

    finish quiz

--- pasja_informatyki/quiz.cpp
+++ pasja_informatyki/quiz.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<fstream>
 #include<cstdlib>
+#include<string>
+#include<algorithm>
 
 using namespace std;
 
@@ -11,6 +13,8 @@ string odpB[5];
 string odpC[5];
 string odpD[5];
 string poprawna[5];
+string odpowiedz;
+int punkty;
 
 int main()
 {
@@ -21,6 +25,7 @@ int main()
 
     
     string linia;
+    int nr_pytania = 0;
 
     if(!plik.good())
     {
@@ -34,10 +39,46 @@ int main()
         {
             case 1: temat = linia; break;
             case 2: nick = linia; break;
-            case 3: tresc[0] = linia; break;
-            
+            case 3: tresc[nr_pytania] = linia; break;
+            case 4: odpA[nr_pytania] = linia; break;
+            case 5: odpB[nr_pytania] = linia; break;
+            case 6: odpC[nr_pytania] = linia; break;
+            case 7: odpD[nr_pytania] = linia; break;
+            case 8: poprawna[nr_pytania] = linia; break;
+        }
+        if(nr_linii == 8)
+        {
+            nr_linii = 2;
+            nr_pytania++;
         }
     }
+
+    plik.close();
+
+    for(int i = 0; i <= 4; i++)
+    {
+        cout << endl << tresc[i] << endl;
+        cout << "A. " << odpA[i] << endl;
+        cout << "B. " << odpB[i] << endl;
+        cout << "C. " << odpC[i] << endl;
+        cout << "D. " << odpD[i] << endl;
+        cout << "Twoja odpowiedź: ";
+        cin >> odpowiedz;
+
+        transform(odpowiedz.begin(), odpowiedz.end(), odpowiedz.begin(), ::tolower);
+        if(odpowiedz == poprawna[i])
+        {
+            cout << "Dobrze!" << endl;
+            punkty++;
+        }
+        else
+        {
+            cout << "Żle!" << endl;
+            cout << "Poprawna odpowiedź to: " << poprawna[i];
+        }
+    }
+
+    cout << endl << "Koniec quizu! Zdobyte punkty: " << punkty << endl;
 
     return 0;
 }
