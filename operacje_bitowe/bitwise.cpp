dd82272b1ea2681e5de6c853b7ba59689f60194b
wiktor <wiktor.kycia@gmail.com>
2025-11-09 12:00:53 +0100

    add function to print bit representation of a char

--- operacje_bitowe/bitwise.cpp
+++ operacje_bitowe/bitwise.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 using namespace std;
 
+void print_bit_representation(char& a)
+{
+    for(int i = 7; i >= 0; i--)
+    {
+        cout << (int)((a & (1 << i)) >> i);
+    }
+    cout << endl;
+}
+
 int main()
 {
     char a = 0b00000001;
@@ -12,24 +21,28 @@ int main()
     cout << get << endl;
 
     // Set
-    n = 1;
+    n = 3;
     a |= 1 << n;
-
     cout << (int)a << endl;
+print_bit_representation(a);
 
 
     // Unset
     a &= ~(1 << n);
-
-    cout << (int)a << endl;
+print_bit_representation(a);
+    // cout << (int)a << endl;
 
     // Toggle
     a ^= 1 << n;
-    cout << (int)a << endl;
+    print_bit_representation(a);
+    // cout << (int)a << endl;
     a ^= 1 << n;
-    cout << (int)a << endl;
+    print_bit_representation(a);
+    // cout << (int)a << endl;
 
     cout << (int)(a & 1) << endl;
+
+    print_bit_representation(a);
 
 
     return 0;
